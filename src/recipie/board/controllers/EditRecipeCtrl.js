1c829224295d8080d57991662f356fd69477c26c
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-05-19 13:16:04 +0300

    Raw version of recePIE

---
+++ src/recipie/board/controllers/EditRecipeCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by INNA on 19.04.2017.
+ */
+app.controller('EditRecipeCtrl', function () {
+    let editRecipe = this;
+
+});
---
+++ src/recipie/recipie.js
@@ -0,0 +1,32 @@
+/**
+ * Created by INNA on 18.04.2017.
+ */
+const app = angular.module('recipieApp', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when ('/',
+            {
+                templateUrl: 'src/recipie/app/cook-book.html',
+                controller: 'CookBookCtrl',
+                controllerAs: 'cookBook'
+            })
+        .when('/recipe/new', //when pressed on Add New button
+            {
+                templateUrl: 'board/tmpl/recipe-edit.html',
+                controller: 'NewRecipeCtrl',
+                controllerAs: 'newRecipe'
+            })
+        .when('/recipe/:id', //when pressed on recipe itself
+            {
+                templateUrl: 'board/tmpl/recipe.html',
+                controller: 'SingleRecipeCtrl',
+                controllerAs: 'singleRecipe'
+            })
+        .when ('/recipe/edit/:id',
+            {
+                templateUrl: 'board/tmpl/recipe-edit.html',
+                controller: 'EditRecipeCtrl',
+                controllerAs: 'editRecipe'
+            })
+        .otherwise({redirectTo: '/'})
+}]);
