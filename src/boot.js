a37408e3c2bfc760ef790a78ab2beaf19ac21c71
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-05-25 12:55:29 +0300

    updated boot.js and package.json files

--- src/boot.js
+++ src/boot.js
@@ -1,3 +1,11 @@
-/**
- * Created by INNA on 25.05.2017.
- */
+head.load(
+    { file: 'lib/jquery.min.js'},
+    { file: 'lib/angular.min.js'},
+    { file: 'lib/urlRouterProvider.js'},
+    { file: 'lib/less.min.js'},
+    { file: 'lib/lodash.min.js'},
+    { file: 'src/recipie/board/controllers/EditRecipeCtrl.js'},
+    { file: 'src/recipie/board/controllers/NewRecipeCtrl.js' },
+    { file: 'src/recipie/board/controllers/SingleRecipeCtrl.js'},
+    { file: 'src/recipie/recipie.js' }
+);
